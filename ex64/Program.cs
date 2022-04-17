8f98a42b855b74d00c37a72278f77ebe74cde73e
Constantin <grishanoff.konstantin@yandex.ru>
2022-04-17 13:55:14 +0500

    ex64 compl

---
+++ ex64/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+
+        {
+
+            // Задайте значения M и N. Напишите программу, которая выведет все 
+            // натуральные числа кратные 3-ём в промежутке от M до N.
+
+            int M = 1;
+            int N = 50;
+
+            Recursia (M, N);
+
+            void Recursia (int numberM, int numberN)
+
+            {
+                if(numberM%3==0) Console.WriteLine(numberM);
+                if(numberM==numberN) return;
+                Recursia (numberM+1, numberN);
+
+            }
+
+        }
+    }
+}
