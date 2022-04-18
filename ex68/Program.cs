75fa2f696b32c31af207b6a64035e5128ef40031
Constantin <grishanoff.konstantin@yandex.ru>
2022-04-18 09:37:45 +0500

    Ex68 compl

---
+++ ex68/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+
+        {
+
+            // Задайте значения M и N. Напишите программу, которая найдёт 
+            // сумму натуральных элементов в промежутке от M до N.
+
+            int M = 3;
+            int N = 3;
+            
+ 
+
+            int Akkerman (int numberM, int numberN)
+
+            {
+                
+                if(numberM==0) return numberN+1 ;
+                else if (numberM>0 && numberN==0) return Akkerman (numberM - 1, 1);
+                else return Akkerman (numberM - 1,  Akkerman (numberM, numberN-1));
+                
+
+            }
+            Console.WriteLine(Akkerman (M, N));
+
+        }
+    }
+}
