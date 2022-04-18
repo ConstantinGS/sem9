9f13eecf642267556421df3f93ced7d782f93cad
Constantin <grishanoff.konstantin@yandex.ru>
2022-04-18 10:35:09 +0500

    Dop1 compl

---
+++ dop1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+
+        { 
+//  Дано предложение. Напишите рекурсивный метод, подсчитывающий
+// количество слов в данном предложении. Словом считается последовательность символов без пробелов.
+
+        
+
+            string str = " Напишите рекурсивный метод,    подсчитывающий...  ";
+            int i = 0;
+            int count = 0;
+            char spaceDel = ' ';
+            str = str.Trim(spaceDel);
+
+            if (str!= " ")
+            {
+            count = 1;
+            }
+            
+            int count_word (string sentence)
+            {
+                if (i == sentence.Length ) return count++;
+                else
+                {
+                    if (sentence[i] == ' ' && sentence[i+1] != ' ' ) count++;
+                    i++;
+                    return count_word (sentence);
+                }
+
+            }
+
+            Console.WriteLine(count_word (str));
+
+
+                
+
+
+ 
+
+
+
+        }
+
+    }
+}
